4c3e990994b35a589cb28009ff64f7a4135b571a
JohnLui <wenhanlv@gmail.com>
2015-05-15 16:03:52 +0800

    clean extra HTTP header

--- Pitaya/Pitaya/Pitaya.swift
+++ Pitaya/Pitaya/Pitaya.swift
@@ -113,11 +113,11 @@ class Pitaya {
         task.resume()
     }
     func buildBody() {
+        let data = NSMutableData()
         if self.files.count > 0 {
             if self.method == "GET" {
                 NSLog("\n\n------------------------\nThe remote server may not accept GET method with HTTP body. But Pitaya will send it anyway.\n------------------------\n\n")
             }
-            let data = NSMutableData()
             for (key, value) in self.params {
                 data.appendData("--\(self.boundary)\r\n".nsdata)
                 data.appendData("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n".nsdata)
@@ -132,10 +132,10 @@ class Pitaya {
                 }
             }
             data.appendData("--\(self.boundary)--\r\n".nsdata)
-            request.HTTPBody = data
         } else if self.params.count > 0 && self.method != "GET" {
-            request.HTTPBody = buildParams(self.params).nsdata
+            data.appendData(buildParams(self.params).nsdata)
         }
+        request.HTTPBody = data
     }
     func buildRequest() {
         if self.method == "GET" && self.params.count > 0 {
@@ -148,7 +148,6 @@ class Pitaya {
         // multipart Content-Type; see http://www.rfc-editor.org/rfc/rfc2046.txt
         if self.files.count > 0 {
             request.addValue("multipart/form-data; boundary=" + self.boundary, forHTTPHeaderField: "Content-Type")
-            request.addValue("form-data", forHTTPHeaderField: "Content-Disposition")
         } else if self.params.count > 0 {
             request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
         }
